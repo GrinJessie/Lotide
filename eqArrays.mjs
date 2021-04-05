b90e32ce813cbdca205bdcaedfcd32c6a45c356f
LijingWang <lijing.wang@energyiq.info>
2021-04-04 23:51:57 -0600

    use recursive to compare arrays and objects deeply

--- eqArrays.mjs
+++ eqArrays.mjs
@@ -1,15 +1,21 @@
 import { assertEqual } from "./assertEqual.mjs";
 
 export const eqArrays = function(array1, array2) {
-  if (array1.length === array2.length) {
-    for (let index = 0; index < array1.length; index++) {
-      if (array1[index] !== array2[index]) {
-        return false;
-      }
-    }
-    return true;
+  if (array1.length !== array2.length) {
+    return false;
   }
-  return false;
+
+  for (let index = 0; index < array1.length; index++) {
+    if(Array.isArray(array1[index]) && Array.isArray(array2[index])) {
+      if(!eqArrays(array1[index], array2[index])) {
+       return false;
+      };
+    } else if (array1[index] !== array2[index]) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
@@ -17,3 +23,8 @@ assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
--- eqObjects.mjs
+++ eqObjects.mjs
@@ -7,10 +7,15 @@ export const eqObjects = (obj1, obj2) => {
     }
 
     for (const key of Object.keys(obj1)) {
-        if (obj1[key] !== obj2[key]) {
-            if(Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
-                return eqArrays(obj1[key], obj2[key]);
-            }
+        if(Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
+            if(!eqArrays(obj1[key], obj2[key])) {
+                return false;
+            };
+        } else if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
+            if (!eqObjects(obj1[key], obj2[key])) {
+                return false;
+            };
+        } else if(obj1[key] !== obj2[key]) {
             return false;
         }
     }
@@ -31,4 +36,8 @@ assertEqual(eqObjects(cd, dc), true);
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false);
 
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
 
