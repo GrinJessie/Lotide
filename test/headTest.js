52f49c0eb09f959380a91adfab7f02ac7e488b95
GrinJessie <lijing@ualberta.ca>
2021-04-06 08:52:08 -0600

    created git ignore file to ignore node modules; install mocha and chai as testing framework to run unit tests

--- test/headTest.js
+++ test/headTest.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 const assertEqual = require('../assertEqual');
 const head = require('../head');
 
@@ -8,3 +9,14 @@ assertEqual(head(["a", "b", "c"]), "a");
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["onlyOne"]), "onlyOne");
 assertEqual(head([]), undefined);
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+});
+
