00af94ec9f17c87d9665ad62a6206c366b91427e
GrinJessie <lijing@ualberta.ca>
2021-04-06 09:18:39 -0600

    Used ES6 Object Value shorthand

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const middle = require('./middle');
+
+const head = head;
+const tail = tail;
+const middle = middle;
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle
+  head,
+  tail,
+  middle
 };
