044762ad1374afee8bbc1c98dbdd19e8cf550793
GrinJessie <lijing@ualberta.ca>
2021-04-06 08:58:42 -0600

    add mocha and chai testing to middle()

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,14 @@
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const assert = require('assert');
 
-
-assertArraysEqual([], []);
+assertArraysEqual(middle([]), []);
 assertArraysEqual(middle([1, 2, 3]), [2]);
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+
+describe("#middle", () => {
+  it("returns [] for []", () => {
+    assert.deepStrictEqual(middle([]), []);
+  });
+});
+  
